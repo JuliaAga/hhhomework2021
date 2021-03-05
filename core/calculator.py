4d65640c6922f458d1d4cec3d22a2ee2b2684c7b
Yuliia <dongro@yandex.ru>
2021-03-05 20:21:49 +0500

    solved

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,42 @@
 from decorators import cache_decorator
+calc = {'+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '/': lambda x, y: x / y if y != 0 else 'division by zero is prohibited',
+        '*': lambda x, y: x * y,
+        '**': lambda x, y: x ** y,
+        }
 
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    return calc.get(operation)(a, b)
+
+
+def getIntInput():
+    while True:
+        try:
+            return int(input('Введите число: '))
+        except Exception:
+            print('Unsupported type of number, try again')
+
+def getOperation():
+    while True:
+        operation = input('Введите операцию: ')
+        if operation in calc:
+            return operation
+        else:
+            print ('Unsupported operation. Supported: + - / * **. Try again')
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+        a = getIntInput() # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+        b = getIntInput()
+        operation = getOperation()
 
-    print('Результат: ', calculator(a, b, operation))
+        print('Результат: ', calculator(a, b, operation))
+
+        if input('Остановить? 1 - да, 0 - нет ') == 1:
+            exit(0)
--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,52 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
-    pass
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    def beep(self):
+        print ("Car {} {} make beep".format(self.brand, self.model))
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars):
+        if cars is None:
+            self.cars = []
+        else:
+            if isinstance(cars, list):
+                self.cars = cars
+            else:
+                print("Need list of cars")
+                exit(-1)
+
+    def __iter__(self):
+        return self.cars.__iter__()
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        self.cars.pop(index)
+
+    def describe(self):
+        print ("There is {} cars in the garage".format(len(self.cars)))
+
+
+if __name__ == '__main__':
+    car1 = Car('Toyota', 'Camry')
+    car2 = Car('Daewoo', 'Matiz')
+    car3 = Car('Opel', 'Astra')
+
+    garage = Garage([car1, car2, car3])
+    garage.describe()
+
+    garage.add(Car('Nissan', ''))
+    garage.delete(1)
+
+    for car in garage:
+        car.beep()
+
+
